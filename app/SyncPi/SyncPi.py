ac73946777338c3e23017852c6da1a81a3855df3
Felipe Morán <felipe.moran11@gmail.com>
2017-09-28 17:54:06 +0200

    Sync working with EMA

--- app/SyncPi/SyncPi.py
+++ app/SyncPi/SyncPi.py
@@ -27,14 +27,27 @@ from SmartMeshSDK.ApiException          import APIError
 
 class MoteClock(object):
     MINIMUM_SLEEP_TIME = 5.8/10.**(5) # sleeping less than this amount of time is useless as the sleep-wakeup routine takes at least this amount of time
+    TARGET_DRIFT_LOOKBACK = 60*5 # time in seconds to look back to calculate the drift coefficient
+    MINIMUM_DRIFT_LOOKBACK = 30 # time in seconds to look back to calculate the drift coefficient
+    EMA_COEFFICIENT = 0.5
 
     def __init__(self, sync_rate): # sync_rate is the rate at which the Raspberry Pi asks the mote the current network timestamp
         self.sync_rate = sync_rate
-        self.offset = 0
         self.keep_thread_alive = False
+        self.drift_coefficient = 1
+        self.reference_list = []
 
+        self._setup_mote()
+
+        self._compensating_drift = False
+        self._compensating_target_drift = False
+        self._did_sync = False
+
+    def __del__(self):
+        self.stop()
+
+    def _setup_mote(self):
         print 'MoteClock from SyncPi'
-
         self.moteconnector = IpMoteConnector.IpMoteConnector()
 
         # self.serialport = raw_input("Enter the serial API port of SmartMesh IP Mote (e.g. COM15): ")
@@ -55,8 +68,13 @@ class MoteClock(object):
                 break
             time.sleep(1)
 
-    def __del__(self):
-        self.stop()
+    @property
+    def early_reference(self):
+        return self.reference_list[-2] # trocar para zero dps
+
+    @property
+    def late_reference(self):
+        return self.reference_list[-1]
 
     def _loop(self):
         print "- Polling mote for timestamp every {}s".format(self.sync_rate)
@@ -68,11 +86,40 @@ class MoteClock(object):
 
     def _sync(self):
         # get network timestamp
-        res = self.moteconnector.send(['getParameter', 'time'], {})
-        network_time = res['utcSecs'] + res['utcUsecs']/10.**6
+        print "Sync! ------------",
+        self._did_sync = True
 
-        # set network timestamp offset in relation to RPi clock
-        self.offset = network_time - time.time()
+        rpi_time = time.time()
+        try:
+            clock_time = self.time()
+        except:
+            clock_time = time.time()
+        response = self.moteconnector.send(['getParameter', 'time'], {})
+        network_time = response['utcSecs'] + response['utcUsecs']/10.**6
+
+        print(" Diff: {} us".format((network_time-clock_time)*10**6))
+
+        self._calculate_drift_coefficient(network_time, rpi_time)
+
+    def _calculate_drift_coefficient(self, network_time, rpi_time):
+        self._add_reference({"network":network_time, "rpi":rpi_time})
+
+        if len(self.reference_list) > self.MINIMUM_DRIFT_LOOKBACK / self.sync_rate:
+            new_drift_coefficient = (rpi_time - self.early_reference["rpi"]) / (network_time - self.early_reference["network"])
+
+            self.drift_coefficient = self.drift_coefficient*(1-self.EMA_COEFFICIENT) + new_drift_coefficient*self.EMA_COEFFICIENT
+            self._compensating_drift = True
+        else:
+            self.drift_coefficient = 1
+            self._compensating_drift = False
+
+    def _add_reference(self, new_reference):
+        self.reference_list.append(new_reference)
+        if len(self.reference_list) > self.TARGET_DRIFT_LOOKBACK/self.sync_rate:
+            self.reference_list.pop(0)
+            self._compensating_target_drift = True
+        else:
+            self._compensating_target_drift = False
 
     def start(self):
         self.thread = threading.Thread(target=self._loop)
@@ -90,7 +137,7 @@ class MoteClock(object):
         print '-  closed everything'
 
     def time(self):
-        return time.time() + self.offset
+        return (time.time() - self.late_reference['rpi']) / self.drift_coefficient + self.late_reference['network']
 
     def sleep(self, seconds):
         time.sleep(seconds)
@@ -104,9 +151,10 @@ class MoteClock(object):
         next_sleep_duration = (time_to_wakeup - self.time())*sleep_factor
 
         while next_sleep_duration > self.MINIMUM_SLEEP_TIME: # this constant is the minimum time a sleep needs to execute, even with a 1 uS argument
+            if next_sleep_duration > 1:
+                print("sleeping {} s".format(next_sleep_duration))
             time.sleep(next_sleep_duration)
             next_sleep_duration = (time_to_wakeup - self.time())*sleep_factor
-
 
 
 
@@ -119,21 +167,30 @@ if __name__ == "__main__":
         print "--- Script already in execution! If you think this is an error delete the file 'lock' from this folder\n\n"
         raise
 
-    clock = MoteClock(sync_rate=30)
-    loop_period = 0.5
+    clock = MoteClock(sync_rate=10)
+    loop_period = 0.2
 
     try:
         clock.start()
-        next_loop = math.ceil(clock.time()) + 0.25 # just a offset, doesn't change much
+        next_loop = math.ceil(clock.time())
 
         pin = 21
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(pin, GPIO.OUT)
-        pin_state = int(next_loop) % 2 == 0 # just a small trick to make sure both pins are in the same phase
+        pin_state = int(next_loop/loop_period) % 2 == 0 # just a small trick to make sure both pins are in the same phase
 
         while True:
             # print "-  Pint {} at instant {:.6f}".format(['HIGH', 'LOW '][pin_state], clock.time())
-            print "{}, {:.6f}".format(['HIGH', 'LOW '][pin_state], clock.time())
+            # print "ps: {}, t.t(): {:.6f}, dc: {:.6f} ppm, lref: {}, ref0: {}".format(['HIGH', 'LOW '][pin_state], time.time(), (1-clock.drift_coefficient)*10**6, len(clock.reference_list), clock.reference_list[0])
+            if clock._did_sync:
+                time.sleep(0.1)
+                print("CompD: {}, TD: {}, LR: {}, D: {:.1f}ppm".format(
+                    clock._compensating_drift,
+                    clock._compensating_target_drift,
+                    len(clock.reference_list),
+                    (1-clock.drift_coefficient)*10**6
+                ))
+                clock._did_sync = False
             GPIO.output(pin, pin_state)
             pin_state = not pin_state
 
