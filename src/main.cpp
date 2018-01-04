b977c6aa2122869f6a2da8ed7acd813951f39910
Felipe Moran <felipe.moran11@gmail.com>
2018-01-04 17:37:37 +0100

    Pin flip non blocking and not using sleep until anymore

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,21 @@
 #define beta 0.2
 #define SLEEP_FACTOR 0.8
 #define MINIMUM_SLEEP_TIME 0 // TODO: review this value
+#define FLIP_DELAY 500000
 
 volatile bool led_status = 0;
+volatile float drift_coef = 1;
+volatile float ema_drift_coef = 1;
+volatile uint32_t last_sync_local_timestamp;
+volatile uint32_t new_sync_local_timestamp;
+volatile uint32_t last_sync_source_timestamp;
+volatile uint32_t new_sync_source_timestamp;
+volatile bool time_to_sync;
 
 void interrupt_routine();
 void sleep_until(uint32_t);
+uint32_t time();
+void flip_pin(int);
 
 void setup() {
     Serial.begin(115200);
@@ -23,14 +33,7 @@ void setup() {
 void loop() {
     // Serial.println("Hey!");
     // delay(1000);
-    static uint32_t next_wakeup = micros() + 500000;
-
-    sleep_until(next_wakeup);
-    next_wakeup += 500000;
-    led_status = !led_status;
-    Serial.print("Flip! ");
-    Serial.println(led_status);
-    digitalWrite(LED_PIN, led_status);
+    flip_pin(LED_PIN);
 
     // Tasks:
         // Get pps signal as interrupt
@@ -42,7 +45,14 @@ void loop() {
 }
 
 void interrupt_routine() {
-    led_status = !led_status;
+    static long pps_counter = 0;
+
+    pps_counter++;
+    if (pps_counter % SYNCHRONIZATION_RATE == 0) {
+        new_sync_local_timestamp = micros();
+        new_sync_source_timestamp = pps_counter * 1000000;
+        time_to_sync = true;
+    }
 }
 
 void sleep_until(uint32_t wakeup_time) {
@@ -51,5 +61,20 @@ void sleep_until(uint32_t wakeup_time) {
         Serial.println(next_sleep_duration);
         delayMicroseconds(next_sleep_duration);
         next_sleep_duration = uint32_t (wakeup_time - micros())*SLEEP_FACTOR;
+    }
+}
+
+uint32_t time() {
+    return (micros()-last_sync_local_timestamp)/drift_coef + last_sync_source_timestamp;
+}
+
+void flip_pin(int pin) {
+    static uint32_t next_wakeup = micros() + 500000;
+    if (time() > next_wakeup) {
+        digitalWrite(LED_PIN, led_status);
+        led_status = !led_status;
+        next_wakeup += 500000;
+        Serial.print("Flip! ");
+        Serial.println(led_status);
     }
 }
