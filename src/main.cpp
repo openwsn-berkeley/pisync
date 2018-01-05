b43faed15f05e99b5ecb8e4c2f6a57961b19fe42
Felipe Moran <felipe.moran11@gmail.com>
2018-01-05 15:35:29 +0100

    Small changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <DueTimer.h>
 
 #define PPS_PIN 2
 #define LED_PIN 13
@@ -16,67 +17,69 @@ volatile uint32_t new_sync_local_timestamp;
 volatile uint32_t last_sync_source_timestamp;
 volatile uint32_t new_sync_source_timestamp;
 volatile bool time_to_sync;
+volatile bool led_pin_state = false;
 
 void interrupt_routine();
 void sleep_until(uint32_t);
 uint32_t time();
-void flip_pin(int);
+void flip_pin_handler();
 void check_sync();
 
 void setup() {
     Serial.begin(115200);
     attachInterrupt(digitalPinToInterrupt(PPS_PIN), interrupt_routine, FALLING);
-
+    Timer3.attachInterrupt(flip_pin_handler).start(FLIP_DELAY);
     pinMode(LED_PIN, OUTPUT);
     Serial.println("Setup complete");
 }
 
 void loop() {
-    // Serial.println("Hey!");
-    // delay(1000);
-    flip_pin(LED_PIN);
+    // flip_pin(LED_PIN);
     check_sync();
-
-    // Tasks:
-        // Get pps signal as interrupt
-        // Calculate drift each SYNC_RATE seconds/pps signals
-        // Make new time source available
-        // In the main thread keep control of the application
-            // Ask for sync (in case of mote)
-            // Toggle pin
 }
 
 void interrupt_routine() {
     static long pps_counter = 0;
+    uint32_t now = micros();
 
     pps_counter++;
     if (pps_counter % SYNCHRONIZATION_RATE == 0) {
-        new_sync_local_timestamp = micros();
+        new_sync_local_timestamp = now;
         new_sync_source_timestamp = pps_counter * 1000000;
         time_to_sync = true;
+
+    } else if (pps_counter % SYNCHRONIZATION_RATE == 1) {
+        Timer3.stop();
+        led_pin_state = false;
+        Timer3.start(FLIP_DELAY/drift_coef);
     }
 }
 
-uint32_t time() {
-    return (micros()-last_sync_local_timestamp)/drift_coef + last_sync_source_timestamp;
-}
+// uint32_t time() {
+//     return (micros()-last_sync_local_timestamp)/drift_coef + last_sync_source_timestamp;
+// }
 
-void flip_pin(int pin) {
-    static const uint32_t DELTA = 10000000;
+// void flip_pin(int pin) {
+//     static const uint32_t DELTA = 10000000;
+//
+//     static int counter;
+//     static bool led_status = false;
+//     static uint32_t next_wakeup = ((time()/1000000) % 1 + 1) * 1000000 + 100000; // added small offter of 100ms so that the sync routine doesn't happen at the same time as the flip
+//
+//     if (time() - next_wakeup + DELTA > DELTA) { // This DELTA is needed because of the micros() rollover each ~71 minutes
+//         digitalWrite(LED_PIN, led_status);
+//         led_status = !led_status;
+//         next_wakeup += 500000;
+//         // Serial.print("Flip! ");
+//         // Serial.print(counter++);
+//         // Serial.print(" time: ");
+//         // Serial.println(time());
+//     }
+// }
 
-    static int counter;
-    static bool led_status = false;
-    static uint32_t next_wakeup = ((time()/1000000) % 1 + 1) * 1000000; // random value
-
-    if (time() - next_wakeup + DELTA > DELTA) { // This DELTA is needed because of the micros() rollover each ~71 minutes
-        digitalWrite(LED_PIN, led_status);
-        led_status = !led_status;
-        next_wakeup += 500000;
-        // Serial.print("Flip! ");
-        // Serial.print(counter++);
-        // Serial.print(" time: ");
-        // Serial.println(time());
-    }
+void flip_pin_handler() {
+    led_pin_state = !led_pin_state;
+    digitalWrite(LED_PIN, led_pin_state);
 }
 
 void check_sync() {
