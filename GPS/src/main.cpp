596df690d1d1ae4c5d05af21b4b0694b41ef7ece
Felipe Moran <felipe.moran11@gmail.com>
2018-01-17 16:27:43 +0100

    Small refactorings

--- GPS/src/main.cpp
+++ GPS/src/main.cpp
@@ -28,12 +28,16 @@ uint32_t time();
 void flip_pin_handler();
 void check_sync();
 void nanoClock_callback();
+void setLedLow();
+void setLedHigh();
+void flip_reset();
 // uint32_t micros_mod();
 
 void setup() {
     Serial.begin(115200);
     attachInterrupt(digitalPinToInterrupt(PPS_PIN), interrupt_routine, FALLING); // PPS interrupt
     Timer3.attachInterrupt(flip_pin_handler).start(FLIP_DELAY); // Pin flip interrupt
+    Timer4.attachInterrupt(flip_reset);
     Timer8.attachInterrupt(nanoClock_callback); // NanoClock callback
     nano_clock.start();
     pinMode(LED_PIN, OUTPUT);
@@ -46,7 +50,7 @@ void loop() {
 }
 
 void nanoClock_callback() {
-    Serial.println("-- NanoClock callback");
+    // Serial.println("-- NanoClock callback");
 }
 
 void interrupt_routine() {
@@ -57,77 +61,60 @@ void interrupt_routine() {
 
     pps_counter++;
     if (pps_counter % SYNCHRONIZATION_RATE == 1) {
-        Serial.println();
-
         if (last_sync == 0) {
-            Serial.println("-- First sync");
             last_sync = now;
             return;
         }
-
-        // DEBUGGING
-        if (now < last_sync) {
-            Serial.println("-- Did rollover");
-        }
-
-        elapsed_useconds_rounded = round(((nano_clock.ticksToNanos(static_cast<uint64_t> (now - last_sync))))/1000000000.)*1000000;
 
         if (now - last_sync == 0) {
             return;
         }
 
-        // DEBUGGING
-        Serial.print("Now: ");
-        Serial.println(now);
-        Serial.print("Last: ");
-        Serial.println(last_sync);
-        Serial.print("Diff: ");
-        Serial.println(now - last_sync);
-
-        Serial.print("Elapsed seconds: ");
-        Serial.println(elapsed_useconds_rounded/1000000);
-
         local_delta = nano_clock.ticksToNanos(static_cast<uint64_t> (now - last_sync))/1000.;
-        source_delta = elapsed_useconds_rounded;
+        source_delta = round(local_delta/1000000)*1000000;
 
         last_sync = now;
         time_to_sync = true;
 
     } else if (pps_counter % SYNCHRONIZATION_RATE == 2) {
-        // Stop timer 3
-        // Arm timer 4 for a reset in 250 ms
-
-        // On timer 4
-        // Flip pin
-        // Arm timer 3
-
-
-        Timer3.stop();
-        Timer3.start(flip_delay_mod);
-
-        REG_PIOB_CODR = 0x1 << 27; // set pin 13 LOW
-        led_pin_state = false;
+        Timer4.start(250000*drift_coef);
     }
 }
 
-void flip_pin_handler() {
-    // led_pin_state = !led_pin_state;
-    // digitalWrite(LED_PIN, led_pin_state);
+void flip_reset() {
+    setLedLow();
+    Timer3.start(flip_delay_mod);
+    led_pin_state = false;
+    Timer4.stop();
+}
 
+void flip_pin_handler() {
     led_pin_state = !led_pin_state;
     if (led_pin_state) {
-        REG_PIOB_SODR = 0x1 << 27; // set pin 13 HIGH
+        setLedHigh();
     } else {
-        REG_PIOB_CODR = 0x1 << 27; // set pin 13 LOW
+        setLedLow();
     }
 
+}
+
+void setLedHigh() {
+    REG_PIOB_SODR = 0x1 << 27; // set pin 13 HIGH
+}
+
+void setLedLow() {
+    REG_PIOB_CODR = 0x1 << 27; // set pin 13 LOW
 }
 
 void check_sync() {
     if (time_to_sync) {
+        time_to_sync = false;
         double instant_drift_coef;
 
         instant_drift_coef = local_delta / source_delta;
+        if (abs(instant_drift_coef-1)*1000*1000 > 100) {
+            return; // drift too big, something must have gone wrong
+        }
 
         if (drift_coef == 0) { // this is the first sync
             ema_drift_coef = instant_drift_coef;
@@ -139,22 +126,16 @@ void check_sync() {
 
         flip_delay_mod = FLIP_DELAY*drift_coef;
 
-        Serial.print("Instant drift: ");
-        Serial.println((instant_drift_coef-1)*1000000);
-        Serial.print("Smooth drift: ");
-        Serial.println((drift_coef-1)*1000000);
-        Serial.print("Local delta: ");
-        Serial.println(local_delta);
-        Serial.print("Source delta: ");
-        Serial.println(source_delta);
-        Serial.print("New delay: ");
-        Serial.println(FLIP_DELAY*drift_coef);
+        // Serial.print("Instant drift: ");
+        // Serial.println((instant_drift_coef-1)*1000000);
+        // Serial.print("Smooth drift: ");
+        // Serial.println((drift_coef-1)*1000000);
+        // Serial.print("Local delta: ");
+        // Serial.println(local_delta);
+        // Serial.print("Source delta: ");
+        // Serial.println(source_delta);
+        // Serial.print("New delay: ");
+        // Serial.println(FLIP_DELAY*drift_coef);
 
-        time_to_sync = false;
     }
 }
-
-// uint32_t micros_mod() {
-//     uint32_t delta = -20*1000*1000;
-//     return micros() + delta;
-// }
